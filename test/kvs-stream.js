56efce90b0a00def252477ee2f595e8d4fd53cb5
Pierre Colle <piercus@gmail.com>
2022-03-14 14:33:44 +0100

    #6 mocked stream should read file content, test counter, test id and type on all ebmlTags in data event

--- test/kvs-stream.js
+++ test/kvs-stream.js
@@ -17,25 +17,26 @@ test('KvsStream', t => {
 		kinesisvideomedia,
 		kinesisvideo,
 	});
-
+	let count = 0;
 	const readStreamPromise = () => new Promise((resolve, reject) => {
-		let ebmlDataTag = {};
 		stream.on('data', fragment => {
-			ebmlDataTag = fragment;
+			t.is(typeof (fragment.id), 'number');
+			t.is(typeof (fragment.type), 'string');
+			t.true(['m', 'u', 'b'].includes(fragment.type), `type ${fragment.type} is not expected as mater tag`);
+
+			count++;
 		});
 
 		stream.on('end', _ => {
-			console.log('Ending Stream');
-			resolve(ebmlDataTag);
+			resolve();
 		});
 
 		stream.on('error', error => {
-			console.log('Error', error);
 			reject(error);
 		});
 	});
 
-	return readStreamPromise().then(ebmlDataTag => {
-		t.is(typeof (ebmlDataTag.id), 'number');
+	return readStreamPromise().then(() => {
+		t.is(count, 251);
 	});
 });
