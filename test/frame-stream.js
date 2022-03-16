07cb1f1c08a6f1060adc568c1d5183139c7b54d3
Dharmik Dalwadi <dharmik.dalwadi@seaflux.tech>
2022-03-16 16:39:33 +0530

    feat: add unit-test for frame object

--- test/frame-stream.js
+++ test/frame-stream.js
@@ -1,4 +1,6 @@
+const fs = require('fs');
 const test = require('ava');
+const beamcoder = require('beamcoder');
 const FrameStream = require('../lib/frame-stream.js');
 const kinesisvideomedia = require('./mock/kinesisvideomedia.js');
 const kinesisvideo = require('./mock/kinesisvideo.js');
@@ -18,7 +20,7 @@ test('FrameStream', t => {
 
 	let count = 0;
 	const readStreamPromise = () => new Promise((resolve, reject) => {
-		stream.on('data', ({tags}) => {
+		stream.on('data', ({ tags }) => {
 			// Tags contains
 			t.is(typeof (tags.AWS_KINESISVIDEO_CONTINUATION_TOKEN), 'string');
 			t.is(typeof (tags.AWS_KINESISVIDEO_FRAGMENT_NUMBER), 'string');
@@ -43,3 +45,43 @@ test('FrameStream', t => {
 	});
 });
 
+test('FrameObject', t => {
+	const getMediaParameters = {
+		StartSelector: {
+			StartSelectorType: 'EARLIEST',
+		},
+		StreamName: 'test-stream',
+	};
+
+	const stream = new FrameStream(getMediaParameters, {
+		kinesisvideomedia,
+		kinesisvideo,
+	});
+
+	const enc = beamcoder.encoder({ // Create an encoder for JPEG data
+		name: 'mjpeg', // FFmpeg does not have an encoder called 'jpeg'
+		width: 1080,
+		height: 1920,
+		pix_fmt: 'yuvj422p',
+		time_base: [1, 1],
+	});
+
+	const firstFramePromise = new Promise((resolve, reject) => {
+		stream.on('data', frame => {
+			stream.destroy();
+			resolve(frame);
+		});
+
+		stream.on('error', error => {
+			reject(error);
+		});
+	});
+
+	return firstFramePromise.then(frame => {
+		t.is(frame.weight, 1920)
+		t.is(frame.height, 1920)
+		t.is(frame.data.length, 12345)
+		t.is(frame.colorspace, 'bt709')
+		return enc.encode(frame)
+	})
+});
