670eccfdbd9ae1413890f925bbe784fe94ffb47e
Dharmik Dalwadi <dharmik.dalwadi@seaflux.tech>
2022-03-21 20:02:04 +0530

    fix: fix the mkv decoding file issue in test-beamcoder-script

--- scripts/test-beamcoder-script.js
+++ scripts/test-beamcoder-script.js
@@ -4,9 +4,10 @@ async function run() {
 	const demuxer = await beamcoder.demuxer('tmp/get_media.mkv');
 	console.log(JSON.stringify(demuxer));
 	const decoder = beamcoder.decoder({ name: 'h264' });
+	decoder.extradata = demuxer.streams[0].codecpar.extradata
 
 	let packet = {};
-	for (let x = 0; x < 1000 && packet !== null; x++) {
+	for (let x = 0; x < 10 && packet !== null; x++) {
 		packet = await demuxer.read();
 		if (packet && packet.stream_index === 0) {
 			const data = await decoder.decode(packet);
@@ -17,7 +18,7 @@ async function run() {
 				pix_fmt: 'yuvj420p',
 				time_base: [1, 1],
 			});
-			const jpeg = enc.encode(data.frames[0])
+			const jpeg = await enc.encode(data.frames[0])
 		}
 	}
 }
