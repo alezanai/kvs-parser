eb7a11ca78763511e49241d7be2669f1242907aa
Dharmik Dalwadi <dharmik.dalwadi@seaflux.tech>
2022-03-21 19:18:33 +0530

    fix: fix typo in file name

--- scripts/test-beamcoder-script.js
+++ scripts/test-beamcoder-script.js
@@ -3,14 +3,21 @@ const beamcoder = require('beamcoder');
 async function run() {
 	const demuxer = await beamcoder.demuxer('tmp/get_media.mkv');
 	console.log(JSON.stringify(demuxer));
-	const decoder = beamcoder.decoder({name: 'h264'});
+	const decoder = beamcoder.decoder({ name: 'h264' });
 
 	let packet = {};
 	for (let x = 0; x < 1000 && packet !== null; x++) {
 		packet = await demuxer.read();
 		if (packet && packet.stream_index === 0) {
-			const frames = await decoder.decode(packet);
-			console.log(frames);
+			const data = await decoder.decode(packet);
+			const enc = beamcoder.encoder({
+				name: 'mjpeg',
+				width: decoder.width,
+				height: decoder.height,
+				pix_fmt: 'yuvj420p',
+				time_base: [1, 1],
+			});
+			const jpeg = enc.encode(data.frames[0])
 		}
 	}
 }
