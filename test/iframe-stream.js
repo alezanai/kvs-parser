92185692e117defff1a6f6913405c5de62cb3209
Dharmik Dalwadi <dharmik.dalwadi@seaflux.tech>
2022-03-28 15:49:28 +0530

    feat: add encoder object in the IFrameStream class

--- test/iframe-stream.js
+++ test/iframe-stream.js
@@ -1,10 +1,11 @@
 const test = require('ava');
+const beamcoder = require('beamcoder');
 const IFrameStream = require('../lib/iframe-stream.js');
 const kinesisvideomedia = require('./mock/kinesisvideomedia.js');
 const kinesisvideo = require('./mock/kinesisvideo.js');
 const logger = require('./helpers/logger.js');
 
-test('TestIFrames', t => {
+test('TestFrameObject', t => {
 	const getMediaParameters = {
 		StartSelector: {
 			StartSelectorType: 'EARLIEST',
@@ -20,6 +21,82 @@ test('TestIFrames', t => {
 
 	let count = 0;
 	stream.on('data', () => {
+		count++;
+	});
+
+	const readIFramesPromise = new Promise((resolve, reject) => {
+		stream.on('end', () => {
+			resolve(count);
+		});
+
+		stream.on('error', error => {
+			reject(error);
+		});
+	});
+
+	return readIFramesPromise.then(count => {
+		t.is(count, 16);
+	});
+});
+
+test('TestFrameJPEG', t => {
+	const getMediaParameters = {
+		StartSelector: {
+			StartSelectorType: 'EARLIEST',
+		},
+		StreamName: 'test-stream',
+	};
+
+	const stream = new IFrameStream(getMediaParameters, {
+		kinesisvideomedia,
+		kinesisvideo,
+		logger,
+	}, 'jpeg');
+
+	let count = 0;
+	stream.on('data', jpeg => {
+		t.is(jpeg.type, 'packets');
+		count++;
+	});
+
+	const readIFramesPromise = new Promise((resolve, reject) => {
+		stream.on('end', () => {
+			resolve(count);
+		});
+
+		stream.on('error', error => {
+			reject(error);
+		});
+	});
+
+	return readIFramesPromise.then(count => {
+		t.is(count, 16);
+	});
+});
+
+test('TestFrameJPEGWithEncoder', t => {
+	const getMediaParameters = {
+		StartSelector: {
+			StartSelectorType: 'EARLIEST',
+		},
+		StreamName: 'test-stream',
+	};
+
+	const stream = new IFrameStream(getMediaParameters, {
+		kinesisvideomedia,
+		kinesisvideo,
+		logger,
+	}, beamcoder.encoder({
+		name: 'mjpeg',
+		width: 1920,
+		height: 1080,
+		pix_fmt: 'yuvj420p',
+		time_base: [1, 1],
+	}));
+
+	let count = 0;
+	stream.on('data', jpeg => {
+		t.is(jpeg.type, 'packets');
 		count++;
 	});
 
