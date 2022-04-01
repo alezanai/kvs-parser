9ec9a5205e72b47e63cf4f9cf6aa3e393fe1fe2b
Pierre Colle <piercus@gmail.com>
2022-04-01 18:38:56 +0200

    fix: #16

--- scripts/kvs-to-frames.js
+++ scripts/kvs-to-frames.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const {Writable} = require('stream');
 const beamcoder = require('beamcoder');
 const FrameStream = require('../lib/frame-stream.js');
 const kinesisvideomedia = require('../test/mock/kinesisvideomedia.js');
@@ -16,22 +17,32 @@ const stream = new FrameStream(getMediaParameters, {
 	kinesisvideo,
 });
 
-let count = 0;
-stream.on('data', frame => {
-	const encoder = beamcoder.encoder({
-		name: 'mjpeg',
-		width: frame.width,
-		height: frame.height,
-		pix_fmt: 'yuvj420p',
-		time_base: [1, 1],
-	});
-	count++;
-	const frameNumber = count;
-	return encoder.encode(frame).then(jpeg => {
-		const filename = `tmp/frame-${frameNumber.toString().padStart(5, '0')}.jpg`;
-		console.log(`Writing file ${filename}`);
-		fs.writeFileSync(filename, jpeg.packets[0].data);
-	}).catch(error => {
-		throw new Error(error);
-	});
-});
+class EncoderStream extends Writable {
+	constructor() {
+		super({objectMode: true});
+		this.encoder = beamcoder.encoder({
+			name: 'mjpeg',
+			width: 1920,
+			height: 1080,
+			pix_fmt: 'yuvj420p',
+			time_base: [1, 1],
+		});
+		this.frameNum = 0;
+	}
+
+	_write(frame, encoding, callback) {
+		this.encoder.encode(frame).then(jpeg => {
+			const filename = `tmp/frame-${this.frameNum.toString().padStart(5, '0')}.jpg`;
+			console.log(`Writing file ${filename}`);
+			this.frameNum++;
+			fs.writeFileSync(filename, jpeg.packets[0].data);
+			callback();
+		}).catch(error => {
+			callback(error);
+		});
+	}
+}
+
+const writable = new EncoderStream();
+
+stream.pipe(writable);
